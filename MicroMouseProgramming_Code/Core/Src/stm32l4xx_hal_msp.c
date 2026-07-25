0d216c2d78014e05f18ec05094385b2dfa1f3f4b
Fred Nicolls <30141109+nicollsf@users.noreply.github.com>
2026-07-25 10:53:28 +0200

    Core: configure TIM4 encoder GPIO inputs with internal Pull-Up resistors to resolve open-drain signal floating

--- MicroMouseProgramming_Code/Core/Src/stm32l4xx_hal_msp.c
+++ MicroMouseProgramming_Code/Core/Src/stm32l4xx_hal_msp.c
@@ -396,7 +396,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     */
     GPIO_InitStruct.Pin = MOTORR_A_ENC_Pin|MOTORR_B_ENC_Pin|MOTORL_A_ENC_Pin|MOTORL_B_ENC_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
